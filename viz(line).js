44d1c0a41c20c3bfc3fd37fca019ada9f545d553
Seven <excalibur44@outlook.com>
2017-06-22 17:38:23 +0800

    rename dashboard(histogram) -> dashboard; dashboard -> dashboard(line)

--- viz(line).js
+++ viz(line).js
@@ -4,7 +4,7 @@
 
 let srcData;
 let mapSvg;
-let histogramSvg;
+let lineSvg;
 let radius;
 let color;
 
@@ -44,8 +44,6 @@ function map(height, width, data) {
     profit.sort(function (x, y) {
         return x - y;
     });
-
-    mapArr = arr;
 
     // 定义半径比例尺
     radius = d3.scale.linear()
@@ -180,128 +178,163 @@ function map(height, width, data) {
     });
 }
 
-function histogram(height, width, data) {
-    const svg = histogramSvg;
+function line(height, width, data) {
+    const svg = lineSvg;
 
-    // 计算每一个子类别对应的总营销额以及利润
+    // 构造时间解析器
+    const parseDate = d3.time.format("%Y/%m/%d").parse;
+    const getMonth  = d3.time.format("%m");
+
+    // 计算每一个类别对应的总营销额以及月份
     let arr = [];
     for (let i = 0; i < data.length; i++){
-        let hasSubCategory = false;
+        let month = getMonth(parseDate(data[i].OrderDate));
+
+        let hasSet = false;
         for (let j = 0; j < arr.length; j++) {
-            if ( data[i].SubCategory === arr[j][0] ) {
-                arr[j][1] += Number(data[i].Sales);
-                arr[j][2] += Number(data[i].Profit);
-                hasSubCategory = true;
+            if ( data[i].Category === arr[j][0] && month === arr[j][1] ) {
+                arr[j][2] += Number(data[i].Sales);
+                hasSet = true;
             }
         }
-        if (!hasSubCategory) {
-            let newArr = [data[i].SubCategory, Number(data[i].Sales), Number(data[i].Profit), data[i].Category];
+        if (!hasSet) {
+            let newArr = [data[i].Category, month, Number(data[i].Sales)];
             arr.push(newArr);
         }
     }
-
-    // arr排序
-    arr.sort(function (x, y) {
-        return y[1] - x[1]
-    });
-    let SubCategory = [], Sales = [], Profit = [];
+    // 按照数据种类提取数据
+    let Category = [], OrderMonth = [], Sales = [];
     for (let i of arr) {
-        SubCategory.push(i[0]);
-        Sales.push(i[1]);
-        Profit.push(i[2]);
+        Category.push(i[0]);
+        OrderMonth.push(i[1]);
+        Sales.push(i[2]);
     }
+    // 按照类别提取数据
+    let OfficeSupplies = [], Technology = [], Furniture = [];
+    for (let i of arr) {
+        if (i[0] === "Office Supplies")
+            OfficeSupplies.push(i);
+        if (i[0] === "Technology")
+            Technology.push(i);
+        if (i[0] === "Furniture")
+            Furniture.push(i);
+    }
+    // 按照月份排序数据
+    OfficeSupplies.sort(function(x, y){
+        return x[1] - y[1];
+    });
+    Technology.sort(function(x, y){
+        return x[1] - y[1];
+    });
+    Furniture.sort(function(x, y){
+        return x[1] - y[1];
+    });
+
+    const color = new Map()
+        .set("Office Supplies", "#e84e40")
+        .set("Technology", "#5677fc")
+        .set("Furniture", "#009688");
 
     // 给坐标轴预留出来空白
     const padding = {left:80, right:80, top:20, bottom:20};
-    // 计算比例尺
-    const xScale = d3.scale.ordinal()
-        .domain(SubCategory)
-        .rangeRoundBands( [0, width - padding.left - padding.right], 0.1 );
-    const yScale = d3.scale.linear()
-        .domain([ 0, parseInt(d3.max(Sales))+1 ])
-        .range([ height - padding.top - padding.bottom, 0 ]);
-    // 颜色比例尺
-    const colorScale = d3.scale.linear()
-        .domain([ parseInt(d3.min(Profit)), 0, parseInt(d3.max(Profit))+1 ])
-        .range(["#e51c23", "#cccccc", "#259b24"]);
+    // 定义坐标轴的比例尺
+    let xScale = d3.scale.linear()
+        .domain(d3.extent(OrderMonth))
+        .range([0, width - padding.left - padding.right]);
+    let yScale = d3.scale.linear()
+        .domain([0, d3.max(Sales)])
+        .range([height - padding.top - padding.bottom, 0]);
 
-    //定义坐标轴
-    const xAxis = d3.svg.axis().scale(xScale).orient("bottom");
-    const yAxis = d3.svg.axis().scale(yScale).orient("left");
+    // 定义坐标轴
+    const xAxis = d3.svg.axis().scale(xScale).orient('bottom');
+    const yAxis = d3.svg.axis().scale(yScale).orient('left');
 
-    //添加坐标轴
-    svg.append("g")
-        .attr("class","axis")
+    // 添加坐标轴
+    svg.append('g')
+        .attr('class', 'axis')
         .attr("transform","translate(" + padding.left + "," + (height - padding.bottom) + ")")
         .call(xAxis)
         // 增加坐标值说明
         .append('text')
-        .text('SubCategory')//子类别
-        .attr('transform', "translate(" + (width - padding.left - padding.right) + ", -7)");
-    svg.append("g")
-        .attr("class","axis")
+        .text('月份')
+        .attr('transform', "translate(" + (width - padding.left - padding.right) + ", -5)");
+    svg.append('g')
+        .attr('class', 'axis')
         .attr("transform","translate(" + padding.left + "," + padding.top + ")")
         .call(yAxis)
         // 增加坐标值说明
         .append('text')
-        .text("Sales")//销售量
+        .text("销售量")
         .attr('transform', "translate(-34, -10)");
 
-    // 添加矩形元素
-    let rect = svg.selectAll("rect")
+    //路径生成器
+    let line = d3.svg.line()
+        .x(function(d) { return xScale(d[1]) + padding.left; })
+        .y(function(d) { return yScale(d[2]) + padding.top; })
+        .interpolate("linear"); // monotone -> 平滑
+
+    // 向svg添加线条
+    svg.append('path')
+        .data(arr)
+        .attr("fill", "transparent")
+        .attr("stroke", color.get("Office Supplies"))
+        .attr('stroke-width', 2)
+        .attr('d', line(OfficeSupplies));
+    svg.append('path')
+        .data(arr)
+        .attr("fill", "transparent")
+        .attr("stroke", color.get("Technology"))
+        .attr('stroke-width', 2)
+        .attr('d', line(Technology));
+    svg.append('path')
+        .data(arr)
+        .attr("fill", "transparent")
+        .attr("stroke", color.get("Furniture"))
+        .attr('stroke-width', 2)
+        .attr('d', line(Furniture));
+
+    let circle = svg.selectAll('circle')
         .data(arr)
         .enter()
-        .append("rect")
-        .attr("transform","translate(" + padding.left + "," + padding.top + ")")
-        .attr("fill", function (d) {
-            return colorScale(d[2]);
+        .append('g')
+        .append('circle')
+        .attr('fill', function (d) {
+            return color.get(d[0])
         })
-        .attr("x", function(d){
-            return xScale(d[0]);
+        .attr('cx', line.x())
+        .attr('cy', line.y())
+        .attr('r', 4);
+
+    circle.on('mouseover', function(d) {
+            d3.select(this).transition().duration(5).attr('r', 6);
+
+            const location = this.getBoundingClientRect();
+            const top  = location.top;
+            const left = (location.left > document.body.clientWidth - 200)
+                ? location.left - 226
+                : location.left + 2;
+
+            // 更新提示条的位置和值，并显示提示条
+            d3.select("#tooltip-line").style("display", "block")
+                .style("left", (left + 5) + "px")
+                .style("top", (top + 5) + "px")
+                .style("background-color", color.get(d[0]));
+
+            // 填充数据
+            d3.select("#Month-line")
+                .text(d[1]);
+            d3.select("#Category-line")
+                .text(d[0]);
+            d3.select("#Sales-line")
+                .text(parseInt(d[2]));
         })
-        .attr("y",function(d){
-            return yScale(d[1])-0.5;
-        })
-        .attr("width", xScale.rangeBand())
-        .attr("height", function(d){
-            return height - padding.top - padding.bottom - yScale(d[1]);
+        .on('mouseout', function() {
+            d3.select(this).transition().duration(5).attr('r', 4);
+            // 隐藏提示条
+            d3.select("#tooltip-line").style("display", "none");
         });
 
-    // 添加鼠标事件
-    rect.on("mouseover", function(d) {
-        //鼠标悬停添加文字
-        //取得鼠标所在元素的坐标
-        const location = this.getBoundingClientRect();
-
-        //控制文字显示位置
-        const x = location.left;
-        // 高于一半则显示在下方，否则显示在上方
-        const fontHeight = 20;
-        const y = (yScale(d[1]) > height/2)
-            ?  innerHeight - 70 - location.height - 14 + 26 - 4*fontHeight
-            :  innerHeight - 70 - location.height - 14 + 26;
-
-        // 更新提示条的位置和值
-        d3.select("#tooltip-histogram").style("display", "block")
-            .style("left", x + "px")
-            .style("top",  y + "px");
-
-        // 填充数据
-        d3.select("#Category-histogram")
-            .text(d[3]);
-        d3.select("#SubCategory-histogram")
-            .text(d[0]);
-        d3.select("#Sales-histogram")
-            .text(parseInt(d[1]));
-        d3.select("#Profit-histogram")
-            .text(parseInt(d[2]));
-    })
-        .on("mouseout", function() {
-            //鼠标移开隐藏文字
-            d3.select("#tooltip-histogram").style("display", "none");
-        });
-
-    rect.on("click", function (d) {
+    circle.on("click", function (d) {
         updateMap(d[0]);
     })
 }
@@ -312,14 +345,14 @@ function updateMap(Category) {
     for (let d of srcData){
         let hasSubCategory = false;
         for (let a of mapArr) {
-            if ( d.State === a[0] && d.SubCategory === a[3] ) {
+            if ( d.State === a[0] && d.Category === a[3] ) {
                 a[1] += Number(d.Sales);
                 a[2] += Number(d.Profit);
                 hasSubCategory = true;
             }
         }
         if (!hasSubCategory) {
-            let newArr = [d.State, Number(d.Sales), Number(d.Profit), d.SubCategory];
+            let newArr = [d.State, Number(d.Sales), Number(d.Profit), d.Category];
             mapArr.push(newArr);
         }
     }
@@ -329,53 +362,21 @@ function updateMap(Category) {
         if (d[3] === Category)
             arr.push(d);
     }
-    //console.log(arr);
-
-    /*mapSvg.selectAll("path")
-        .data(arr)
-        .attr("fill", function (d) {
-            const location = this.getBoundingClientRect();
-            //选择悬停元素的父元素svg，并向里面添加圆点
-            d3.select(this.parentNode)
-                .datum(d)
-                .append('g')
-                .append('circle')
-                .attr('cx', location.left + location.width/2)
-                .attr('cy', location.top + location.height/2)
-                .attr('r', function (d) {
-                    let sales = Math.abs(d.properties.Sales);
-                    return radius(sales);
-                })
-                .attr("fill", function(d) {
-                    let profit = Math.log(Math.abs(d.properties.Profit));
-                    if (d.properties.Profit > 0)
-                        return color(profit);
-                    else
-                        return color(-profit);
-                })
-                .attr("opacity", 0.76);
-
-            return "#cccccc";
-        });*/
-
-    radius.domain([
-        d3.min(arr, function(d) { return Math.abs(parseFloat(d[1])); }),
-        d3.max(arr, function(d) { return Math.abs(parseFloat(d[1])); })
-    ]);
 
     mapSvg.selectAll("circle")
         .data(arr)
+        .transition().duration(500)
+        .attr('r', function (d) {
+            let sales = Math.abs(d[1]);
+            return radius(sales);
+        })
+        .transition().duration(500)
         .attr("fill", function(d) {
             let profit = Math.log(Math.abs(d[2]));
             if (d[2] > 0)
                 return color(profit);
             else
                 return color(-profit);
-        })
-        .transition().duration(500)
-        .attr('r', function (d) {
-            let sales = Math.abs(d[1]);
-            return radius(sales);
         });
 
     mapSvg.selectAll("circle")
